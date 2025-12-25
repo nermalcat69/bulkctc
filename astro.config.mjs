86587b2a57c3a9038fed0d400670b3b8da95c492
BMO <meow@nermalcat69.dev>
2025-12-25 23:37:47 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://bulkctc.com',
   integrations: [mdx(), sitemap()],
   adapter: vercel(),
 });
