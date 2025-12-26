2fc057e4ce44b58aef3bbd45c2f1aed45c57e3d9
BMO <meow@nermalcat69.dev>
2025-12-26 22:41:12 +0530

    add privacy page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 import vercel from '@astrojs/vercel';
 
@@ -10,5 +11,8 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://bulkctc.com',
   integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   adapter: vercel(),
 });
